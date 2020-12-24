765412d68cbc973b1225e110e10b7858977dc2ae
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-12-24 17:51:08 -0500

    feat(src)

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,3 @@
-let userInfo: string;
-let username: string = 'anbreaker';
-let phone: number;
-// phone = '+367128815'; --> Error de tipo
-phone = 924234669;
-let isPro: boolean;
-isPro = true;
-username = 'Javier'
-userInfo = `
-    User info:
-    username: ${username}
-    firtsname: ${username + ' anbreaker'}
-    phone: ${phone}
-    isPro: ${isPro}
-`;
-
-console.log(userInfo);
+import express from 'express';
+const app = express();
+export default app;
