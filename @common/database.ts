acc1aa1fb326335080778fe6f2d00381ad19c11c
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-12-27 20:21:09 -0500

    feat(@common): add new config to connection

--- @common/database.ts
+++ @common/database.ts
@@ -1,7 +1,13 @@
 import { connect } from 'mongoose';
+const { DB_INDICATIVE, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_HOST, NODE_ENV, DB_OPTIONS } = process.env;
 
 export const startConnection = async () => {
-  await connect(`mongodb://localhost:${process.env.DB_DATABASE}`, {
+  let stringConnection = `mongodb://localhost:${DB_PORT}/${process.env.DB_DATABASE}`;
+
+  if (NODE_ENV === 'production')
+    stringConnection = `mongodb${DB_INDICATIVE}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}${DB_OPTIONS}`;
+
+  await connect(stringConnection, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
