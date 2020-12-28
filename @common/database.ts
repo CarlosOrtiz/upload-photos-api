f8a8a3eb9525a1481526bf7cb46113eed5ced543
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-12-27 19:45:55 -0500

    fix(@common): add new config by connection

--- @common/database.ts
+++ @common/database.ts
@@ -3,6 +3,9 @@ import { connect } from 'mongoose';
 export const startConnection = async () => {
   await connect(`mongodb://localhost:${process.env.DB_DATABASE}`, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
   })
   console.log('Database is connection')
 }
