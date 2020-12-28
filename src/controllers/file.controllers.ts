cb86c15d0095d65da17fa93b4ff82c35259f20e3
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-12-27 19:46:22 -0500

    feat(controllers,router): new crub photo

--- src/controllers/file.controllers.ts
+++ src/controllers/file.controllers.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from 'express'
-import moment from 'moment';
 import Photo from '../models/Photo';
+import moment from 'moment';
+import fse from 'fs-extra';
+import path from 'path';
+import fs from 'fs';
 
-export const getAllPhotos = (req: Request, res: Response): Response => {
-
-  return res.json({ success: 'OK' })
+export const getAllPhotos = async (req: Request, res: Response): Promise<Response> => {
+  return res.json(await Photo.find({ state: 'active' }))
 }
 
+export const getAllPhotosInactive = async (req: Request, res: Response): Promise<Response> => {
+  return res.json(await Photo.find({ state: 'inactive' }))
+}
 
-export const createPhoto = (req: Request, res: Response): Response => {
-  console.log('Savaing file')
+export const getPhotoById = async (req: Request, res: Response): Promise<Response> => {
+  return res.json(await Photo.findById(req.params.id))
+}
+
+export const createPhoto = async (req: Request, res: Response): Promise<Response> => {
   const { name, detail } = req.body;
   const newFile = {
     name,
@@ -19,8 +27,52 @@ export const createPhoto = (req: Request, res: Response): Response => {
     date: moment().format("DD MM YYYY h:mm:ss a")
   }
   const data = new Photo(newFile)
-
-  console.log(data)
+  await data.save();
 
   return res.json({ success: 'OK' })
 }
+
+export const deletePhoto = async (req: Request, res: Response): Promise<Response> => {
+  const data = await Photo.findByIdAndRemove(req.params.id);
+  if (!data)
+    return res.json({ error: 'PHOTO_NOT_EXIT', detail: "¡La foto no se encuentra registrada!" })
+
+  if (fs.existsSync(path.resolve(data.url)))
+    await fse.unlink(path.resolve(data.url))
+
+  return res.json({ success: 'OK' })
+}
+
+export const updatedPhoto = async (req: Request, res: Response): Promise<Response> => {
+  const { name, detail } = req.body;
+  try {
+    const response = await Photo.findByIdAndUpdate(req.params.id, {
+      name, detail
+    });
+
+    if (!response)
+      return res.json({ error: 'PHOTO_NOT_EXIT', detail: "¡La foto no se encuentra registrada!" })
+
+    return res.json({ success: 'OK' })
+  } catch (err) {
+    return res.json({ err })
+  }
+
+}
+
+export const inactivePhoto = async (req: Request, res: Response): Promise<Response> => {
+  await Photo.findByIdAndUpdate(req.params.id, {
+    state: "inactive"
+  });
+
+  return res.json({ success: 'OK' })
+}
+
+export const activePhoto = async (req: Request, res: Response): Promise<Response> => {
+  await Photo.findByIdAndUpdate(req.params.id, {
+    state: "active"
+  });
+
+  return res.json({ success: 'OK' })
+}
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,33 @@
 import { Router } from 'express';
 const router = Router();
-import { createPhoto, getAllPhotos } from '../controllers/file.controllers'
+import {
+  createPhoto,
+  getAllPhotos,
+  getPhotoById,
+  deletePhoto,
+  updatedPhoto,
+  inactivePhoto,
+  getAllPhotosInactive,
+  activePhoto
+} from '../controllers/file.controllers'
 import multer from '../../@common/multer';
 
 router.route('/photos')
   .post(multer.single('image'), createPhoto)
   .get(getAllPhotos)
+
+router.route('/photos/inactive')
+  .get(getAllPhotosInactive)
+
+router.route('/photos/:id')
+  .get(getPhotoById)
+  .delete(deletePhoto)
+  .put(updatedPhoto)
+
+router.route('/photos/inactive/:id')
+  .put(inactivePhoto)
+
+router.route('/photos/active/:id')
+  .put(activePhoto)
 
 export default router;
